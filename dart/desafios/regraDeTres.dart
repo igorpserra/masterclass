0347a4ef0e3ff844d34d1a4b82fbc7a8a6a851b2
igorpserra <igorpserra@gmail.com>
2022-10-19 02:04:51 -0300

    regra de 3

--- dart/desafios/regraDeTres.dart
+++ dart/desafios/regraDeTres.dart
@@ -1,5 +1,14 @@
 void main(List<String> args) {
-  regraDeTres(fator1: 1, fator2: 2);
+  regraDeTres(fator1: 50, fator2: 100, fator4: 5);
 }
 
-void regraDeTres({int? fator1, int? fator2, int? fator3, int? fator4}) {}
+void regraDeTres(
+    {double fator1 = 0,
+    double fator2 = 0,
+    double fator3 = 0,
+    double fator4 = 0}) {
+  double part1 = (fator1 * fator4) / (fator2 + fator3);
+  double part2 = (fator2 * fator3) / (fator1 + fator4);
+
+  print('x = ${part1 + part2}');
+}
