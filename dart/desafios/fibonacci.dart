369d6a2452653e5ebfa2b7cc12f37ce5856f5c4b
igorpserra <igorpserra@gmail.com>
2022-10-19 09:08:29 -0300

    recursive fibonacci

--- dart/desafios/fibonacci.dart
+++ dart/desafios/fibonacci.dart
@@ -1,16 +1,26 @@
 void main(List<String> args) {
-  print(fibonacci(15));
+  print(fibonacci(6));
 }
 
+List<int> fib = [];
+
 List<int> fibonacci(int reps) {
-  if (reps == 0) return [];
-  if (reps == 1) return [0];
-  if (reps == 2) return [0, 1];
-  List<int> retorno = [0, 1];
-  int fator = 1;
-  for (int i = 3; i <= reps; i++) {
-    fator = retorno[retorno.length - 2] + retorno[retorno.length - 1];
-    retorno.add(fator);
+  if (reps == 0) return fib;
+  if (fib.length < 1) {
+    fib.add(0);
+    reps--;
+    fibonacci(reps);
+    return fib;
   }
-  return retorno;
+  if (fib.length == 1) {
+    fib.add(1);
+    reps--;
+    fibonacci(reps);
+    return fib;
+  }
+
+  fib.add(fib[fib.length - 2] + fib[fib.length - 1]);
+  reps--;
+  fibonacci(reps);
+  return fib;
 }
